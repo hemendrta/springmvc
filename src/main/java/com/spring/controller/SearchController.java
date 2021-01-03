6ddc55bff8c2c2b663acc2f6badde88f62e96478
hemendrta <hemendra.singh.c@outlook.com>
2021-01-02 21:27:02 -0800

    Search controller example

---
+++ src/main/java/com/spring/controller/SearchController.java
@@ -0,0 +1,33 @@
+package com.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SearchController {
+
+//	handler to show the search page
+	@RequestMapping("/search")
+	public String getSearch() {
+
+		return "search";
+
+	}
+
+//	handler to pass the search string which we want to search in the google.
+	@RequestMapping("/searchQuery")
+	public RedirectView handleSearch(@RequestParam("query") String query) {
+
+//		concatenating the value which we receive from the form with the query which we want to pass.
+		String searchQuery = "https://www.google.com/search?q=" + query;
+
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl(searchQuery);
+
+		return redirectView;
+
+	}
+
+}
