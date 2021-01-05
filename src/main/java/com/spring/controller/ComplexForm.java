c1be2446ede1b3782d646f09c9bbf505d15a6610
hemendrta <hemendra.singh.c@outlook.com>
2021-01-04 20:30:37 -0800

    file uploading with commonsMultipartFile

--- src/main/java/com/spring/controller/ComplexForm.java
+++ src/main/java/com/spring/controller/ComplexForm.java
@@ -1,12 +1,17 @@
 package com.spring.controller;
 
+import java.io.FileOutputStream;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.model.Address;
@@ -100,7 +105,8 @@ public class ComplexForm {
 	}
 
 	@RequestMapping(path = "/handleComplexForm2", method = RequestMethod.POST)
-	public ModelAndView handleComplexForm2(@ModelAttribute("person") Person person, BindingResult bindingResult) {
+	public ModelAndView handleComplexForm2(@ModelAttribute("person") Person person, BindingResult bindingResult,
+			HttpSession session) {
 		ModelAndView modelAndView = new ModelAndView();
 		if (bindingResult.hasErrors()) {
 			modelAndView.setViewName("complexForm2");
@@ -111,6 +117,50 @@ public class ComplexForm {
 
 		}
 
+		return modelAndView;
+	}
+
+	/* file upload example */
+	@RequestMapping("/fileUpload")
+	public ModelAndView fileUpload() {
+
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("fileform");
+		return modelAndView;
+
+	}
+
+	/* handling file upload */
+	@RequestMapping(path = "/fileUploadHandler", method = RequestMethod.POST)
+	public ModelAndView fileUploadHandler(@RequestParam("fileUpload") CommonsMultipartFile multipartFile,
+			HttpSession httpSession) {
+		ModelAndView modelAndView = new ModelAndView();
+		System.out.println(multipartFile.getOriginalFilename());
+		System.out.println(multipartFile.getContentType() + "\n" + multipartFile.getSize());
+
+		// getting the file in the bytes format.
+		byte[] data = multipartFile.getBytes();
+
+		// specifying the path where we want to save the file which we have uploaded. We
+		// can change the path as per
+		// our requirement.
+		String path = httpSession.getServletContext().getRealPath("/") + multipartFile.getOriginalFilename();
+
+		// writing the file
+		try {
+			FileOutputStream fileOutputStream = new FileOutputStream(path);
+			fileOutputStream.write(data);
+
+			System.out.println(path);
+
+			modelAndView.setViewName("success");
+		}
+
+		catch (Exception e) {
+
+			System.out.println("error uploading!!");
+			modelAndView.setViewName("fileform");
+		}
 		return modelAndView;
 	}
 
---
+++ src/main/webapp/WEB-INF/views/fileform.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+	crossorigin="anonymous">
+
+<title>File Upload Example</title>
+</head>
+<body class="bg-secondary">
+
+	<div class="container mx-auto">
+
+		<div class="card mx-auto px-4" style="margin-top: 80px; width: 70%">
+			<h5 class="text-center mt-3">File Upload Example</h5>
+			<div class="card-body">
+				<form action="fileUploadHandler" method="post"
+					enctype="multipart/form-data">
+					<div class="form-group">
+						<label for="fileUpload">Upload File</label> <input
+							class="form-control-file" type="file" id="fileUpload"
+							name="fileUpload">
+						<button class="btn btn-outline-success mt-3">Upload</button>
+					</div>
+				</form>
+			</div>
+		</div>
+
+	</div>
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+		integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+		crossorigin="anonymous"></script>
+	<script
+		src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+		integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+		crossorigin="anonymous"></script>
+	<script
+		src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+		integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+		crossorigin="anonymous"></script>
+</body>
+</html>
